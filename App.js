37f2b8ac5fb66306efcf0c6fa23413248bb61bce
Felipe Oliveira <felipe.o.silva18@gmail.com>
2018-03-16 18:26:38 -0300

    add components directory

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View } from 'react-native'
-import DeckStatusBar from './src/DeckStatusBar';
-import Tabs from './src/Tabs'
+import DeckStatusBar from './src/components/DeckStatusBar'
+import Tabs from './src/components/Tabs'
+// import { fetchDecks } from './utils/api'
 
-const App = () => (
-  <View style={{flex: 1}}>
-    <DeckStatusBar backgroundColor={'#aaa'} />
-    <Tabs />
-  </View>
-)
+class App extends Component {
+  // componentDidMount() {
+  //   fetchDecks()
+  //     .then(decks => this.props.dispatch(receiveDecks(decks)))
+  // }
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <DeckStatusBar backgroundColor={'#aaa'} />
+        <Tabs />
+      </View>
+    )
+  }
+}
 
 export default App
